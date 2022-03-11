dbd332c9d8e98df2897d080c54009f6d1e93708d
M-viz <86562227+M-viz@users.noreply.github.com>
2022-03-11 18:03:47 +0100

    Update RW.R

--- RW.R
+++ RW.R
@@ -1,4 +1,4 @@
-#function to ease tasks involving rolling window
+#function to ease tasks involving rolling window.
 
 # i= number of iteration (length of the window)  
 fun="sqrt" # A character variable including the name of the function that you created or that you want to use for the rolling window
